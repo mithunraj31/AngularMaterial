daddd90334d48bf35300ec0dec598860bea08b41
lasitha <mbel002@mbel.co.jp>
2019-11-29 15:32:54 +0900

    intercept bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -58,6 +58,7 @@ import { OutgoingShipmentsComponent } from './pages/shipments/outgoing-shipments
 import { LoginComponent } from './pages/login/login.component';
 import { AuthGuardService } from './auth/AuthGuardService';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -134,7 +135,12 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     IncomingShipmentService,
     AuthService,
     AuthGuardService,
-    AuthInterceptorService
+    AuthInterceptorService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,18 +10,28 @@ export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>,
     next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const idToken = localStorage.getItem("id_token");
-
-    if (idToken) {
-      const cloned = req.clone({
-        headers: req.headers.set("Authorization",
-          "Bearer " + idToken)
-      });
-
-      return next.handle(cloned);
-    }
-    else {
-      return next.handle(req);
-    }
+    
+      const idToken = localStorage.getItem("id_token");
+      if(idToken){
+        
+        const authReq = req.clone({
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + idToken,
+            'Access-Control-Allow-Origin': '*'
+          })
+        });
+    
+        return next.handle(authReq);
+      } else {
+        const authReq = req.clone({
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          })
+        });
+    
+        return next.handle(authReq);
+      }
   }
 }

925f72dc1873d7f7c725fd9863e241a1d38f3527
lasitha <mbel002@mbel.co.jp>
2020-02-13 15:55:38 +0900

    Auth settings for users menu

--- src/app/pages/my-page/my-page.component.ts
+++ src/app/pages/my-page/my-page.component.ts
@@ -13,72 +13,14 @@ export class MyPageComponent implements OnInit {
   passwordForm: FormGroup;
   progress = false;
   successPasswordChange: boolean;
-  constructor(private auth: AuthService,
-    private userService: UserService,
-    private _snackBar: MatSnackBar) { }
-  firstName;
-  lastName;
+  constructor(private auth: AuthService,) { }
+
   role;
-  passwordError: boolean = false;
-  changeError = null;
+
   ngOnInit() {
-    this.initializeLoginForm();
-    this.firstName = this.auth.getFirstName();
-    this.lastName = this.auth.getLastName();
     this.role = this.auth.getRole();
   }
 
-  initializeLoginForm() {
-    this.passwordForm = new FormGroup({
-      "oldPassword": new FormControl("", [
-        Validators.required,
-      ]),
-      "newPassword": new FormControl("", [
-        Validators.required
-      ]),
-      "confirmPassword": new FormControl("", [
-        Validators.required
-      ])
-
-    })
-  }
-  onSubmit() {
-    this.progress = true;
-    this.passwordError = false;
-    if (this.passwordForm.value.newPassword != this.passwordForm.value.confirmPassword) {
-      this.passwordError = true;
-      this.progress = false;
-    } else {
-      this.userService.updatePassword(this.passwordForm.value).subscribe(success => {
-        this.successPasswordChange = true;
-        this.progress = false;
-        this.openSnackBar("Your password has been changed successfully!", "ok");
-        // this.passwordForm.reset();
-        this.passwordForm.markAsPristine();
-        this.passwordForm.markAsUntouched();
-        this.passwordForm.updateValueAndValidity();
-        this.changeError = null;
-      }, error => {
-        if (error && error.status ) {
-          if(error.status == 401){
-          this.changeError = 401;
-        } else if(error.status == 400) {
-          this.changeError = 400;
-        }
-        } else {
-          this.changeError = 500;
-        }
-        this.progress = false;
-      })
-      this.passwordError = false;
-    }
-  }
-
-  openSnackBar(message: string, action: string) {
-    this._snackBar.open(message, action, {
-      duration: 2000,
-    });
-  }
-
+ 
 
 }

5a81a93377081be60635551e6eab50816b05b400
lasitha <mbel002@mbel.co.jp>
2020-02-25 11:46:06 +0900

    envirement changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  APIURL : "http://13.113.194.21:4000/mbel"
+  APIURL : "http://13.113.194.21:4000/mbel",
+  APPNAME : "MBEL CLient - staging"
 };
 
 /*

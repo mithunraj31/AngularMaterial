b5fdc2b6c37c7b1f418c881ec0fab27c7f893bf9
lasitha <mbel002@mbel.co.jp>
2020-06-04 13:46:55 +0900

    sortOrderURL

--- src/app/services/OrderService.ts
+++ src/app/services/OrderService.ts
@@ -15,7 +15,7 @@ export class OrderService {
 
     getOrders(checkBox: SortCheckbox) {
         return this.http.get<Order[]>
-        (this.getOrdersUrl+"?fcst="+checkBox.fcst+"?wait="+checkBox.wait+"?withKitting="+checkBox.withKitting+"?withoutKitting="+checkBox.withoutKitting);
+        (this.getOrdersUrl+"?fcst="+checkBox.fcst+"&&?wait="+checkBox.wait+"&&?withKitting="+checkBox.withKitting+"&&?withoutKitting="+checkBox.withoutKitting);
     }
     getFulfilledOrders() {
         return this.http.get<Order[]>(this.getOrdersUrl + 'fulfilled/');
